04e732d67ac8e7afb690df43b06a342b82c24ceb
GrayGooGlitch <maxwell@brokengif.com>
2025-03-22 20:42:02 -0400

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,47 @@
+AltTexture({
+    key = 'tarot_tex',
+    set = 'Tarot',
+    path = 'Tarots.png',
+    keys = {
+        'c_fool',
+        'c_magician',
+        'c_high_priestess',
+        'c_empress',
+        'c_emperor',
+        'c_heirophant',
+        'c_lovers',
+        'c_chariot',
+        'c_justice',
+        'c_hermit',
+        'c_wheel_of_fortune',
+        'c_strength',
+        'c_hanged_man',
+        'c_death',
+        'c_temperance',
+        'c_devil',
+        'c_tower',
+        'c_star',
+        'c_moon',
+        'c_sun',
+        'c_judgement',
+        'c_world'
+    },
+    original_sheet = 'true',
+    loc_txt = {
+      name = 'Orgre Battle Tarot'
+    }
+})
+
+TexturePack({
+    key = 'obt_tarot_pack',
+    textures = {
+      'obt_tarot_tex'
+    },
+    loc_txt = {
+      name = 'Ogre Battle Tarot',
+      text = {'Ogre Battle Tarot Cards'}
+    }
+})
+
+----------------------------------------------
+------------MOD CODE END----------------------
